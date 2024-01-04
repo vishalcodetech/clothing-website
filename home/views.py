106ac94e57939941aa5cae59eed289af8e1e285b
vishalcodetech <vishalcodetech@gmail.com>
2024-01-04 17:49:46 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+   
+    return render(request,'index.html')
+
+def product(request):
+    return render(request,'products.html')
+
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
